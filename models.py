9440ced588e93c800dee0872569446c263effbda
yonghyun <younghyun347@gmail.com>
2024-10-16 15:43:48 +0900

    db 변경

--- models.py
+++ models.py
@@ -2,8 +2,9 @@ from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 class Todo(Base):
-    __tablename__ = "todos"
+    __tablename__ = "todos" 
+
 
     id = Column(Integer, primary_key=True, index=True)
-    task = Column(String, nullable=False)
+    task = Column(String(255), nullable=False)  # 255는 최대 길이 예시
     completed = Column(Boolean, default=False)
